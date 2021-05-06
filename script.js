6dbd1504968321c4c3c8361addd1f81d34b9b0c7
mercspring <68706508+mercspring@users.noreply.github.com>
2021-05-06 14:00:03 -0700

    added rules uploading and reading

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const csvFile = document.getElementById("csv-input");
+const rulesFile = document.getElementById("rules-input");
+let rules = []
 
 csvFile.onchange = event => {
     csvFile.files[0].text().then(function(text){
-        console.log(text)
+        const result = $.csv.toObjects(text);
+        console.log(result);
+    })
+}
+
+rulesFile.onchange = event => {
+    rulesFile.files[0].text().then(function(text){
+        rules = JSON.parse(text);
+        console.log(rules);
     })
 }
