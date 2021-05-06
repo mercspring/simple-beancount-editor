f9c28f1ea3b0b1184a3b3b8299e49296ed156308
mercspring <68706508+mercspring@users.noreply.github.com>
2021-05-06 14:50:47 -0700

    added the rendering of the transactions table

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 const csvFile = document.getElementById("csv-input");
 const rulesFile = document.getElementById("rules-input");
 let rules = []
+let transactions = []
+
+function appendTransactionsToTable() {
+    console.log("got to function")
+    for(let i = 0; i < transactions.length; i++) {
+        console.count("looping")
+        const transaction = `<tr data-transaction-number=${i}>
+            <td>${transactions[i].Description}</td>
+            <td>${transactions[i]["Transaction Date"]}</td>
+        </tr>`;
+        console.log(transaction);
+        $("#beancount-table").append(transaction)
+
+    }
+
+}
 
 csvFile.onchange = event => {
-    csvFile.files[0].text().then(function(text){
-        const result = $.csv.toObjects(text);
-        console.log(result);
+    csvFile.files[0].text().then(function (text) {
+        transactions = $.csv.toObjects(text);
+
+        console.log(transactions);
+        appendTransactionsToTable();
     })
 }
 
 rulesFile.onchange = event => {
-    rulesFile.files[0].text().then(function(text){
+    rulesFile.files[0].text().then(function (text) {
         rules = JSON.parse(text);
         console.log(rules);
     })
