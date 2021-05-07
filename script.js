a0d0b87e7d64a533f488271dec709f8f4c1929cb
mercspring <68706508+mercspring@users.noreply.github.com>
2021-05-07 14:02:36 -0700

    added color coding of table rows based on if an account has been assigned to a transaction

--- script.js
+++ script.js
@@ -4,47 +4,79 @@ const accountsFile = document.getElementById("accounts-input");
 let rules = [];
 let transactions = [];
 let accounts = [];
-
+let currentTransaction = 0;
 function appendTransactionsToTable() {
+    console.log("appending transactions")
+    $('#beancount-table').empty();
     for (let i = 0; i < transactions.length; i++) {
         const transaction = $(`<tr class="transaction-row" data-transaction-number=${i}>
             <td>${transactions[i].Description}</td>
             <td>${transactions[i]["Transaction Date"]}</td>
         </tr>`);
+
+        if(transactions[i].account != 'No Account'){
+            transaction.addClass('blue');
+        } else {
+            transaction.addClass('red');
+
+        }
+
+
+
         $("#beancount-table").append(transaction)
 
     }
     $(".transaction-row").on("click", function () {
-        const transaction = transactions[$(this).data("transaction-number")];
-        console.log(transaction);
+        currentTransaction = $(this).data("transaction-number")
+        const transaction = transactions[currentTransaction];
+        console.log(transaction, currentTransaction);
         $('#transaction-details .description').html(transaction.Description);
         $('#transaction-details .date').html(transaction["Transaction Date"]);
         $('#transaction-details .amount-one').html(transaction.Amount * -1);
         $('#transaction-details .amount-two').html(transaction.Amount * 1);
-
-
+        // $('.account-one').attr("data-transaction-number", transactionNumber);
+        $('#account-select').val(transaction.account);
     })
 
 }
 
-function poplulateDrawnAccount(){
-    const drawnAccounts = accounts.filter(line => line.match(/Liabilities|Assets/))
-    for (let i = 0; i < drawnAccounts.length; i++){
+function populateAccounts() {
+
+    for (let i = 0; i < accounts.length; i++) {
         const option = $(`<option value=${accounts[i]}>${accounts[i]} </option>`)
+        $('#account-select').append(option);
+    }
+}
+
+function populateDrawnAccount() {
+    const drawnAccounts = accounts.filter(line => line.match(/Liabilities|Assets/))
+
+    for (let i = 0; i < drawnAccounts.length; i++) {
+        const option = $(`<option value=${drawnAccounts[i]}>${drawnAccounts[i]} </option>`)
         $('#drawn-account').append(option);
     }
 }
 
-$('#drawn-account').on('change', function(){
+$('#drawn-account').on('change', function () {
     $('.account-two').html($('#drawn-account').val())
 })
 
+$('#account-select').on('change', function () {
+
+    console.log($(this).val());
+    transactions[currentTransaction].account = $('#account-select').val();
+    appendTransactionsToTable();
+})
 
 // File input Handlers
 csvFile.onchange = event => {
     csvFile.files[0].text().then(function (text) {
         console.log(text);
         transactions = $.csv.toObjects(text);
+        transactions = transactions.map(transaction => {
+            transaction.account = 'No Account';
+            return transaction;
+        })
 
         console.log(transactions);
         appendTransactionsToTable();
@@ -54,9 +86,28 @@ csvFile.onchange = event => {
 rulesFile.onchange = event => {
     rulesFile.files[0].text().then(function (text) {
         rules = JSON.parse(text);
-        console.log(rules);
     })
 }
+
+$('#apply-rules-button').click(function () {
+    if (rules.length > 1) {
+        console.log('found some rules');
+        for (let i = 0; i < transactions.length; i++) {
+            for (let j = 0; j < rules.length; j++) {
+                const pattern = new RegExp(rules[j].keyword, 'i');
+                console.log(rules[j].keyword, transactions[i].Description)
+                if (pattern.test(transactions[i].Description)) {
+                    if (rules[j].account != '') {
+                        transactions[i].account = rules[j].account;
+                    }
+                }
+            }
+        }
+
+    }
+    appendTransactionsToTable();
+
+})
 
 accountsFile.onchange = event => {
     accountsFile.files[0].text().then(function (text) {
@@ -69,8 +120,8 @@ accountsFile.onchange = event => {
             } else {
                 return null
             }
-        }).filter(line => line && line.match(/Liabilities|Assets/))
-        poplulateDrawnAccount();
+        }).filter(line => line)
+        populateDrawnAccount();
         populateAccounts();
 
     })
